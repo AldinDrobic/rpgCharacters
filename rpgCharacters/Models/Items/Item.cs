c798845c0040ca5fba388de24e18403872a1ecdf
Aldin Drobic <aldin.drobic@hotmail.se>
2022-02-11 17:46:26 +0100

    CHORE: Updated variables in item classes

--- rpgCharacters/Models/Items/Item.cs
+++ rpgCharacters/Models/Items/Item.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using rpgCharacters.Models.Items;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,13 +11,39 @@ namespace rpgCharacters.Models
     {
         private string ItemName;
         private int ItemLvl;
+        private ItemSlot ItemSlot;
         
 
-        public Item(string itemName, int itemLvl)
+        public Item(string itemName, int itemLvl, ItemSlot itemSlot)
         {
             this.ItemName = itemName;
             this.ItemLvl = itemLvl;
+            this.ItemSlot = itemSlot;
         }
 
+        /// <summary>
+        /// Returns items name
+        /// </summary>
+        /// <returns></returns>
+        public string getItemName()
+        {
+            return this.ItemName;
+        }
+        /// <summary>
+        /// Returns items required lvl
+        /// </summary>
+        /// <returns></returns>
+        public int getRequiredLvl()
+        {
+            return this.ItemLvl;
+        }
+        /// <summary>
+        /// Returns items slot
+        /// </summary>
+        /// <returns></returns>
+        public ItemSlot GetItemSlot()
+        {
+            return this.ItemSlot;
+        }
     }
 }
--- rpgCharacters/Models/Items/Weapon.cs
+++ rpgCharacters/Models/Items/Weapon.cs
@@ -12,11 +12,34 @@ namespace rpgCharacters.Models
         private WeaponTypes WeaponType;
         private double Damage;
         private double AttackSpeed;
-        public Weapon(string itemName, int itemLvl, Enum itemSlot, Enum weaponType) 
+        /// <summary>
+        /// Constructor to instantiate a object.
+        /// </summary>
+        /// <param name="itemName">Name of the item eg. "Axe"</param>
+        /// <param name="itemLvl">Required level to use item</param>
+        /// <param name="itemSlot">In which slot is item equipped, eg "Slot.Weapon"</param>
+        /// <param name="weaponTypes">What type of weapon, eg. "Axe"</param>
+        public Weapon(string itemName, int itemLvl, ItemSlot itemSlot, WeaponTypes weaponTypes) 
             :base(itemName, itemLvl, itemSlot)
         {
         }
 
+        /// <summary>
+        /// Returns weapon type
+        /// </summary>
+        /// <returns></returns>
+        public WeaponTypes getWeaponType()
+        {
+            return this.WeaponType;
+        }
+        /// <summary>
+        /// Calculates weapons damage and then returns damage.
+        /// </summary>
+        /// <returns></returns>
+        public double getDPS()
+        {
+            return this.Damage * this.AttackSpeed;
+        }
        
     }
 }
