7dfb91849bffd23e4d9fad57c8f3777879fd3de9
Aldin Drobic <aldin.drobic@hotmail.se>
2022-02-11 18:06:27 +0100

    FEAT: Added variables and methods to PrimaryAttributes class

--- rpgCharacters/Models/Attributes/PrimaryAttributes.cs
+++ rpgCharacters/Models/Attributes/PrimaryAttributes.cs
@@ -6,7 +6,48 @@ using System.Threading.Tasks;
 
 namespace rpgCharacters.Models
 {
-    internal class PrimaryAttributes
+    public class PrimaryAttributes
     {
+        public int Strength { get; set; }
+        public int Dexterity { get; set; }
+        public int Intelligence { get; set; }
+
+        /// <summary>
+        /// Initializable constructor.
+        /// </summary>
+        /// <param name="strength"></param>
+        /// <param name="dexterity"></param>
+        /// <param name="intelligence"></param>
+        public PrimaryAttributes(int strength, int dexterity, int intelligence)
+        {
+            this.Strength = strength;
+            this.Dexterity = dexterity;
+            this.Intelligence = intelligence;
+        }
+
+        /// <summary>
+        /// Will increase strength with what ever value is passed in the parameter.
+        /// </summary>
+        /// <param name="increaseValue"></param>
+        public void increaseStrength(int increaseValue)
+        {
+            this.Strength += increaseValue;
+        }
+        /// <summary>
+        /// Will increase strength with what ever value is passed in the parameter.
+        /// </summary>
+        /// <param name="increaseValue"></param>
+        public void increaseDexterity(int increaseValue)
+        {
+            this.Dexterity += increaseValue;
+        }
+        /// <summary>
+        /// Will increase strength with what ever value is passed in the parameter.
+        /// </summary>
+        /// <param name="increaseValue"></param>
+        public void increaseIntelligence(int increaseValue)
+        {
+            this.Intelligence += increaseValue;
+        }
     }
 }
