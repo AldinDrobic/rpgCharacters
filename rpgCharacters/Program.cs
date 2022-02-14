75c4f440296dfe2db05a15ea342324af901689f5
Aldin Drobic <aldin.drobic@hotmail.se>
2022-02-14 12:08:00 +0100

    CHORE: classes testWeapon and testMage

--- rpgCharacters/Program.cs
+++ rpgCharacters/Program.cs
@@ -2,6 +2,7 @@
 using rpgCharacters.Models.Characters;
 using rpgCharacters.Models.Exceptions;
 using rpgCharacters.Models.Items;
+using rpgCharacters.TestData;
 using System;
 using System.Collections.Generic;
 
@@ -11,18 +12,8 @@ namespace rpgCharacters
     {
         static void Main(string[] args)
         {
-
-            Weapon weapon1 = new Weapon("STAFF", 1, ItemSlot.WEAPON, WeaponTypes.STAFF, 1, 1);
-            Weapon weapon = new Weapon("AXE", 1, ItemSlot.WEAPON, WeaponTypes.AXE, 1, 1);
-            Mage mage = new Mage("Aldin", 1, 1, 1);
-
-
-            mage.EquipWeapon(weapon);
-
-            //weapons.Add(new WeaponTypes());
-
-
-
+            TestMageData.testMage1.EquipWeapon(TestWeaponData.getTestStaff());
+            TestMageData.testMage1.EquipWeapon(TestWeaponData.getTestBow());
         }
     }
 }
---
+++ rpgCharacters/TestData/TestWeaponData.cs
@@ -0,0 +1,51 @@
+﻿using rpgCharacters.Models;
+using rpgCharacters.Models.Items;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rpgCharacters.TestData
+{
+    public static class TestWeaponData
+    {
+        private static Weapon Axe = new Weapon("AXE", 1, ItemSlot.WEAPON, WeaponTypes.AXE, 1, 1);
+        private static Weapon Bow = new Weapon("BOW", 1, ItemSlot.WEAPON, WeaponTypes.BOW, 1, 1);
+        private static Weapon Dagger = new Weapon("DAGGER", 1, ItemSlot.WEAPON, WeaponTypes.DAGGER, 1, 1);
+        private static Weapon Hammer = new Weapon("HAMMER", 1, ItemSlot.WEAPON, WeaponTypes.HAMMER, 1, 1);
+        private static Weapon Staff = new Weapon("STAFF", 1, ItemSlot.WEAPON, WeaponTypes.STAFF, 1, 1);
+        private static Weapon Sword = new Weapon("Sword", 1, ItemSlot.WEAPON, WeaponTypes.SWORD, 1, 1);
+        private static Weapon Wand = new Weapon("Wand", 1, ItemSlot.WEAPON, WeaponTypes.WAND, 1, 1);
+
+
+        public static Weapon getTestAxe()
+        {
+            return Axe;
+        }
+        public static Weapon getTestBow()
+        {
+            return Bow;
+        }
+        public static Weapon getTestDagger()
+        {
+            return Dagger;
+        }
+        public static Weapon getTestHammer()
+        {
+            return Hammer;
+        }
+        public static Weapon getTestStaff()
+        {
+            return Staff;
+        }
+        public static Weapon getTestSword()
+        {
+            return Sword;
+        }
+        public static Weapon getTestWand()
+        {
+            return Wand;
+        }
+    }
+}
