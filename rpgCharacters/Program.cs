c53dc9d5ae5e941ae14cfa96f2aadad63d78e2b9
Aldin Drobic <aldin.drobic@hotmail.se>
2022-02-16 20:36:22 +0100

    FIX: Cleaned project som unused code and comments

--- rpgCharacters/Program.cs
+++ rpgCharacters/Program.cs
@@ -1,13 +1,8 @@
 ﻿using rpgCharacters.Models;
 using rpgCharacters.Models.Characters;
-using rpgCharacters.Models.Exceptions;
 using rpgCharacters.Models.Items;
-using rpgCharacters.TestData;
-using rpgCharacters.TestData.TestItems;
 using System;
-using System.Collections.Generic;
 using rpgCharacters.Models.Attributes;
-using rpgCharacters.TestData.TestCharacters;
 
 namespace rpgCharacters
 {
@@ -15,14 +10,7 @@ namespace rpgCharacters
     {
         static void Main(string[] args)
         {
-            Warrior warrior = new Warrior();
-            Weapon axe = new Weapon("Axe", 1, ItemSlot.WEAPON, WeaponTypes.AXE, new WeaponAttributes(7, 1.1));
-            Armor armor = new Armor("Plate armor", 1, ItemSlot.BODY, ArmorType.PLATE, new ArmorAttributes(1));
-            warrior.EquipWeapon(axe);
-            warrior.EquipArmor(armor);
-            Console.WriteLine(warrior);
-            warrior.DealDamage();
-            var damage = warrior.GetCharacterDamage();
+            //To test this project please use the xUnit test that can be found in the solution named "ApplicationTests";
         }
     }
 }
